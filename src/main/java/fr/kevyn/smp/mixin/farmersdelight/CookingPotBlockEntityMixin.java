efa3cfb753c1cb5968842b35a5a5bf5fa68ac630
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-05-22 00:56:43 +0200

    feat(astage): refactor cooking pot mixin

--- src/main/java/fr/kevyn/smp/mixin/farmersdelight/CookingPotBlockEntityMixin.java
+++ src/main/java/fr/kevyn/smp/mixin/farmersdelight/CookingPotBlockEntityMixin.java
@@ -3,7 +3,6 @@ package fr.kevyn.smp.mixin.farmersdelight;
 import java.util.Optional;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,52 +13,46 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.neoforged.neoforge.items.wrapper.RecipeWrapper;
 import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
 import vectorwing.farmersdelight.common.crafting.CookingPotRecipe;
 
 @Mixin(CookingPotBlockEntity.class)
 public abstract class CookingPotBlockEntityMixin {
-  @Shadow
-  private RecipeManager.CachedCheck<RecipeWrapper, CookingPotRecipe> quickCheck;
-
-  @Shadow
-  protected abstract boolean hasInput();
-
   @Inject(method = "getMatchingRecipe", at = @At("RETURN"), cancellable = true)
   private void injectGetMatchingRecipe(RecipeWrapper inventoryWrapper,
       CallbackInfoReturnable<Optional<RecipeHolder<CookingPotRecipe>>> cir) {
-    CookingPotBlockEntity self = (CookingPotBlockEntity) (Object) this;
-    var level = self.getLevel();
+    var recipe = cir.getReturnValue();
 
-    if (level != null && this.hasInput()) {
-      var recipe = this.quickCheck.getRecipeFor(inventoryWrapper, level);
-      recipe.ifPresent(r -> {
-        CookingPotRecipe instance = r.value();
-        @SuppressWarnings("null")
-        ItemStack stack = instance.getResultItem(null);
+    recipe.ifPresent(r -> {
+      BlockEntity self = (BlockEntity) (Object) this;
+      var level = self.getLevel();
+      if (level == null)
+        return;
 
-        var server = level.getServer();
-        if (server == null)
-          return;
+      var server = level.getServer();
+      if (server == null)
+        return;
 
-        if (!self.getPersistentData().hasUUID("smp:owner"))
-          return;
+      if (!self.getPersistentData().hasUUID("smp:owner"))
+        return;
 
-        Player player = server.getPlayerList().getPlayer(self.getPersistentData().getUUID("smp:owner"));
-        if (player == null)
-          return;
+      Player player = server.getPlayerList().getPlayer(self.getPersistentData().getUUID("smp:owner"));
+      if (player == null)
+        return;
 
-        var restriction = ARestrictionManager.RECIPE_INSTANCE.getRestriction(player,
-            new com.alessandro.astages.core.wrapper.RecipeWrapper(
-                RecipeType.simple(ResourceLocation.fromNamespaceAndPath("farmersdelight", "cooking")),
-                ResourceLocation.parse(stack.getItem().toString())));
+      @SuppressWarnings("null")
+      ItemStack stack = r.value().getResultItem(null);
 
-        if (restriction != null)
-          cir.setReturnValue(Optional.empty());
-      });
-    }
+      var restriction = ARestrictionManager.RECIPE_INSTANCE.getRestriction(player,
+          new com.alessandro.astages.core.wrapper.RecipeWrapper(
+              RecipeType.simple(ResourceLocation.fromNamespaceAndPath("farmersdelight", "cooking")),
+              ResourceLocation.parse(stack.getItem().toString())));
+
+      if (restriction != null)
+        cir.setReturnValue(Optional.empty());
+    });
   }
 }
